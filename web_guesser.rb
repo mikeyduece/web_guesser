07e037461f7f73acc11ff63f4e51fdc2b0f0b3ba
Mike Heft <mikeheft@gmail.com>
2017-08-06 10:25:57 -0600

    Add background color changing

--- web_guesser.rb
+++ web_guesser.rb
@@ -18,8 +18,21 @@ def check(guess)
   end
 end
 
+def background(message)
+  if message == "Way too High!" || message == "Way too low!"
+    "#de1738"
+  elsif message == "Too high!" || message == "Too low!"
+    "#ffdbe0"
+  elsif message == "You got it right!"
+    "#32cd32"
+  else
+    "white"
+  end
+end
+
 get '/' do
   guess   = params["guess"]
   message = check(guess.to_i)
-  erb :index, locals: {number: settings.number, message: message}
+  background = background(message)
+  erb :index, locals: {number: settings.number, message: message, background: background}
 end
